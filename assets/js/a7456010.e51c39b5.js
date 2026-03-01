81c40bc84ccc1ee5d928b09da8a3b42b5a239783
Sratique <196660851+Sratique@users.noreply.github.com>
2026-03-01 18:39:09 +0100

    Deploy website - based on 20bb09fafef092e129c6243298cbf515a241feef

--- assets/js/a7456010.e51c39b5.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkinterface_works_docs=globalThis.webpackChunkinterface_works_docs||[]).push([[235],{8552(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
